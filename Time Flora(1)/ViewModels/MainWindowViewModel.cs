2e5624a0b239409d10df03171f6727080e2623a9
mitul72 <mituldhawan2@gmail.com>
2023-05-28 18:01:01 -0600

    Added a progress bar and button for timer

--- Time Flora(1)/ViewModels/MainWindowViewModel.cs
+++ Time Flora(1)/ViewModels/MainWindowViewModel.cs
@@ -2,9 +2,16 @@
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
         public string AngularBrackLeft => "<";
         public string AngularBrackRight => ">";
+        public int progress { get; set; } = 100;
+        public int minutes { get; set; } = 20;
+        public int seconds { get; set; } = 0;
+        public int hours { get; set; } = 0;
+        public int days { get; set; } = 0;
+        public int months { get; set; } = 0;
+        public int years { get; set; } = 0;
+        public string minuteSeconds { get; set; } = "20:00";
     }
 }
 
--- Time Flora(1)/Views/MainWindow.axaml.cs
+++ Time Flora(1)/Views/MainWindow.axaml.cs
@@ -1,12 +1,20 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Time_Flora_1_.Views
 {
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Angular_bracket_left_OnClick(object? sender, RoutedEventArgs e)
+        {
+            // throw new System.NotImplementedException();
+            
         }
     }
 }
