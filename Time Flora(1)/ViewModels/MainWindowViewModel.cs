e633d9284c7918204730157b52a1098e3dae22af
mitul72 <mituldhawan2@gmail.com>
2023-07-12 20:57:40 -0600

    updated background, added button functionality, added circular progress bar

--- Time Flora(1)/ViewModels/MainWindowViewModel.cs
+++ Time Flora(1)/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,29 @@
-﻿namespace Time_Flora_1_.ViewModels
+﻿using System.ComponentModel;
+using Avalonia.Interactivity;
+using Avalonia.Rendering;
+
+namespace Time_Flora_1_.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
         public string AngularBrackLeft => "<";
         public string AngularBrackRight => ">";
         public int progress { get; set; } = 100;
-        public int minutes { get; set; } = 20;
-        public int seconds { get; set; } = 0;
+        public static int minutes { get; set; } = 20;
+        public static int seconds { get; set; } = 0;
         public int hours { get; set; } = 0;
         public int days { get; set; } = 0;
         public int months { get; set; } = 0;
         public int years { get; set; } = 0;
-        public string minuteSeconds { get; set; } = "20:00";
+        public string test;
+        public string minuteSeconds { get; set; } = $"{minutes}:{seconds.ToString("00")}";
+
+        public void updateBinding()
+        {
+            minutes -= 10;
+            this.minuteSeconds = $"{minutes}:{seconds.ToString("00")}";
+        }
+
     }
 }
-
--- Time Flora(1)/Views/MainWindow.axaml.cs
+++ Time Flora(1)/Views/MainWindow.axaml.cs
@@ -1,20 +1,47 @@
+using System.ComponentModel;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Time_Flora_1_.ViewModels;
 
 namespace Time_Flora_1_.Views
 {
-    public partial class MainWindow : Window
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        MainWindowViewModel viewModel = new MainWindowViewModel();
+
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         private void Angular_bracket_left_OnClick(object? sender, RoutedEventArgs e)
         {
             // throw new System.NotImplementedException();
-            
+            // viewModel.updateBinding();
+            if (ViewModels.MainWindowViewModel.minutes == 5)
+            {
+                return;
+            }
+
+            ViewModels.MainWindowViewModel.minutes -= 5;
+            TimerButton.Content = $"{MainWindowViewModel.minutes}:{MainWindowViewModel.seconds.ToString("00")}";
+
+        }
+
+        private void Angular_bracket_right_OnClick(object? sender, RoutedEventArgs e)
+        {
+            // throw new System.NotImplementedException();
+            if (ViewModels.MainWindowViewModel.minutes == 120)
+            {
+                return;
+            }
+
+            ViewModels.MainWindowViewModel.minutes += 5;
+            TimerButton.Content = $"{MainWindowViewModel.minutes}:{MainWindowViewModel.seconds.ToString("00")}";
         }
     }
 }
